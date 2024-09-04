835406085aabb3ec8664c122ac87b8369436797e
Renee Messersmith <morganrmessersmith@gmail.com>
2024-09-04 17:21:10 -0400

    Feat: show, create, update actions

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,9 +1,27 @@
 class Api::V1::PostersController < ApplicationController
     def index
-        render json: Poster.all
+        posters = Poster.all
+        render json: PosterSerializer.format_posters(posters)
     end
 
     def show
-        render json: Poster.find(params[:id])
+        poster = Poster.find(params[:id])
+        render json: PosterSerializer.format_poster(poster)
+    end
+
+    def create
+        new_poster = Poster.create(poster_params)
+        render json: PosterSerializer.format_poster(new_poster)
+    end
+
+    def update
+        updated_poster = Poster.update(params[:id], poster_params)
+        render json: PosterSerializer.format_poster(updated_poster)
+    end
+    
+    private
+    
+    def poster_params
+        params.require(:poster).permit(:name, :description, :price, :year, :vintage,  :img_url)
     end
 end
