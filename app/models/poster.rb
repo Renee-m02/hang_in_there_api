1edf37b195dca73e14bfeba5516a69b0eaaa7d91
Candice Cirbo <thecirbos@icloud.com>
2024-09-06 16:35:27 -0600

    Feat refactors poster model and minor formatting cleanup

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -10,9 +10,8 @@ class Poster < ApplicationRecord
 
     def self.find_by_params(params)
         all_posters = all
-
         if params[:name]
-            results = filter_by_name(all_posters, params[:name])
+           results = filter_by_name(all_posters, params[:name])
         end
 
         if params[:min_price]
@@ -22,19 +21,20 @@ class Poster < ApplicationRecord
         if params[:max_price]
             results = filter_by_max(all_posters, params[:max_price])
         end
-
         results
     end
 
+    private
+    
     def self.filter_by_name(posters, name)
         posters.where("name ILIKE '%#{name}%'").order(:name)
     end
 
     def self.filter_by_min(posters, min_price)
-        posters.where("price <= #{min_price}")
+        posters.where("price >= #{min_price}")
     end
 
     def self.filter_by_max(posters, max_price)
-        posters.where("price >= #{max_price}")
+        posters.where("price <= #{max_price}")
     end
 end
