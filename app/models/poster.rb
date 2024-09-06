989530c244bc9f04ab29d717f02cc707ccd3142f
Candice Cirbo <thecirbos@icloud.com>
2024-09-06 14:03:34 -0600

    Test adds test file for model methods

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,3 +1,15 @@
 class Poster < ApplicationRecord
-    
+    def self.sort_by_creation(order = all)
+        if order == 'desc'
+          order(created_at: :desc)
+        # elsif order == 'desc'
+        #   order(created_at: :desc)
+        else
+          all
+        end
+    end
+
+    # def self.total_count
+    #     count
+    # end
 end
